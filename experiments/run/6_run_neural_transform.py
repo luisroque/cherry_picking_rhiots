f70d3ad15f8b0430b7fd187e9a950b1f22f69b5c
Luís Roque <luisroque@MacBook-Pro-3.lan>
2024-10-30 00:03:39 +0000

    correcting bugs on requirements and on importing data with correct dtypes for running neural transforms

--- experiments/run/6_run_neural_transform.py
+++ experiments/run/6_run_neural_transform.py
@@ -33,24 +33,32 @@ for data_name, groups in DATASETS_FREQ.items():
 
         for transformation in transformations:
             for version in range(1, n_versions + 1):
-                print(f"Processing {data_name} - {group} | Transformation: {transformation}, Version: {version}")
+                print(
+                    f"Processing {data_name} - {group} | Transformation: {transformation}, Version: {version}"
+                )
 
                 transformed_file_path = f"{TRANSFORMED_DATA_DIR}/{data_name}/{data_name}_{group}_{transformation}_v{version}.csv"
                 output_file_path = f"{RESULTS_DIR}/{data_name}_{group}_{transformation}_v{version}_neural.csv"
 
                 if os.path.exists(output_file_path):
                     print(
-                        f"Output file for {data_name} - {group} - {transformation} - v{version} already exists. Skipping...")
+                        f"Output file for {data_name} - {group} - {transformation} - v{version} already exists. Skipping..."
+                    )
                     continue
 
                 try:
-                    ds = pd.read_csv(transformed_file_path)
+                    ds = pd.read_csv(
+                        transformed_file_path,
+                        dtype={"unique_id": str, "y": float},
+                        parse_dates=["ds"],
+                    )
                 except FileNotFoundError as e:
                     print(
-                        f"Error loading transformed data for {data_name} - {group} - {transformation} - v{version}: {e}")
+                        f"Error loading transformed data for {data_name} - {group} - {transformation} - v{version}: {e}"
+                    )
                     continue
 
-                n_series = ds['unique_id'].nunique()
+                n_series = ds["unique_id"].nunique()
 
                 models = [
                     AutoRNN(h=h, cpus=15, gpus=0),
@@ -68,8 +76,11 @@ for data_name, groups in DATASETS_FREQ.items():
                     cv_nf = cv_nf.reset_index()
 
                     cv_nf.to_csv(output_file_path, index=False)
-                    print(f"Results saved for {data_name} - {group} - {transformation} - v{version}")
+                    print(
+                        f"Results saved for {data_name} - {group} - {transformation} - v{version}"
+                    )
 
                 except Exception as e:
                     print(
-                        f"Error during cross-validation for {data_name} - {group} - {transformation} - v{version}: {e}")
+                        f"Error during cross-validation for {data_name} - {group} - {transformation} - v{version}: {e}"
+                    )
